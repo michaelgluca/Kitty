45cda2cf415a87d5f89e8026656593addc28a7c2
Michael Luca <88971408+SH13LD0@users.noreply.github.com>
2023-07-29 13:31:29 +0100

    MVC & Assets

---
+++ Kitty/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  Kitty
+//
+//  Created by Michael Luca on 27/07/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}

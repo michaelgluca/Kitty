d84f5ab7e694ecb07aaf4e1a02069017d3246284
michaelgluca <michael.luca.msc@gmail.com>
2023-08-13 20:57:30 +0100

    add ElinkDetailedView

---
+++ Kitty/Model/ExtLinkData.swift
@@ -0,0 +1,8 @@
+//
+//  ExtLinkData.swift
+//  Kitty
+//
+//  Created by Michael on 13/08/2023.
+//
+
+import Foundation
---
+++ Kitty/View/ElinkDetailedView.swift
@@ -0,0 +1,18 @@
+//
+//  elinkDetailedView.swift
+//  Kitty
+//
+//  Created by Michael on 13/08/2023.
+//
+
+import SwiftUI
+
+struct elinkDetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    elinkDetailedView()
+}

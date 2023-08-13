e22737074ce229eba496c0766a62ff6bd53ddcfd
michaelgluca <michael.luca.msc@gmail.com>
2023-08-13 21:45:20 +0100

    add external links

--- Kitty/View/SetupView.swift
+++ Kitty/View/SetupView.swift
@@ -9,45 +9,45 @@ import SwiftUI
 
 struct SetupView: View {
     var body: some View {
-     
-            NavigationView {
-
-                    List {
-                        Section {
-                            Text("Guardians Setup")
-                                .listRowBackground(Capsule().fill(Color(red: 0.815, green: 0.496, blue: 0.847, opacity: 0.9)))
-                                .foregroundColor(Color.white)
-                                .font(Font.body)
-                                .bold()
-                                .listRowSeparator(.hidden)
-                                .padding()
-                            Text("Location Access")
-                                .listRowBackground(Capsule().fill(Color(red: 0.815, green: 0.496, blue: 0.847, opacity: 0.9)))
-                                .foregroundColor(Color.white)
-                                .font(Font.body)
-                                .bold()
-                                .listRowSeparator(.hidden)
-                                .padding()
-                            Text("App Passcode")
-                                .listRowBackground(Capsule().fill(Color(red: 0.815, green: 0.496, blue: 0.847, opacity: 0.9)))
-                                .foregroundColor(Color.white)
-                                .font(Font.body)
-                                .bold()
-                                .listRowSeparator(.hidden)
-                                .padding()
-                        } header: {
-                            Text("Emergency settings setup")
-                                .font(.title)
-                                .bold()
-                                .foregroundColor(.accentColor)
-                                .shadow(radius: 5)
-                        }
-                    }
-                    .scrollContentBackground(.hidden)
-                    .background{
-                        Image(.background)
-                    }
+        
+        NavigationView {
+            
+            List {
+                Section {
+                    Text("Guardians Setup")
+                        .listRowBackground(Capsule().fill(Color(red: 0.815, green: 0.496, blue: 0.847, opacity: 0.9)))
+                        .foregroundColor(Color.white)
+                        .font(Font.body)
+                        .bold()
+                        .listRowSeparator(.hidden)
+                        .padding()
+                    Text("Location Access")
+                        .listRowBackground(Capsule().fill(Color(red: 0.815, green: 0.496, blue: 0.847, opacity: 0.9)))
+                        .foregroundColor(Color.white)
+                        .font(Font.body)
+                        .bold()
+                        .listRowSeparator(.hidden)
+                        .padding()
+                    Text("App Passcode")
+                        .listRowBackground(Capsule().fill(Color(red: 0.815, green: 0.496, blue: 0.847, opacity: 0.9)))
+                        .foregroundColor(Color.white)
+                        .font(Font.body)
+                        .bold()
+                        .listRowSeparator(.hidden)
+                        .padding()
+                } header: {
+                    Text("Emergency settings setup")
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.accentColor)
+                        .shadow(radius: 5)
+                }
             }
+            .scrollContentBackground(.hidden)
+            .background{
+                Image(.background)
+            }
+        }
     }
 }
 
